cd4d4e06426937067205df01c81bd2426345517a
arvindrag <crabtree.arvind@gmail.com>
2018-07-29 22:55:57 -0700

    curtail bad dloads

--- putio_util.py
+++ putio_util.py
@@ -54,7 +54,7 @@ class PutIO:
 					if xfer['transfer']['status'] == 'COMPLETED':
 						self.logger.info("xfer completed")
 						fileid = xfer['transfer']['file_id']
-					if xfer['down_speed'] == 0:
+					if xfer['transfer']['down_speed'] == 0:
 						return None
 				else:
 					self.logger.info("returning name, link")
