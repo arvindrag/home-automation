382ec1a6ad30c23a954572a465faddd8e5221f51
arvindrag <crabtree.arvind@gmail.com>
2018-09-09 14:42:41 -0700

    fix logging better

--- cast_bot.py
+++ cast_bot.py
@@ -110,7 +110,7 @@ def setup():
 	# get an instance of the logger object this module will use
 	logger = logging.getLogger(__name__)
 	# instantiate the JournaldLogHandler to hook into systemd
-	journald_handler = JournaldHandler()
+	journald_handler = JournalHandler()
 	# set a formatter to include the level name
 	journald_handler.setFormatter(logging.Formatter('[%(levelname)s] %(message)s'))
 	# add the journald handler to the current logger
