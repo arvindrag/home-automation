94bcb0e9f96a63026e07964310502f835d31b8a7
arvindrag <crabtree.arvind@gmail.com>
2019-04-15 22:04:00 -0700

    add soup to parsing

--- tpb_parser.py
+++ tpb_parser.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 import re
 
 class TPBParser:
@@ -7,17 +8,17 @@ class TPBParser:
 		self.logger = logger
 
 	def get_page(self, encoded_term):
-		url = self.API+'/search/{}/0/99/0'.format(encoded_term)
+		url = self.API+'/search/{}'.format(encoded_term)
 		self.logger.info("Scraping {}".format(url))
 		resp = requests.get(url)
-		return resp.text.replace('>','>\n')
+		return resp.text
 
-	def get_tag_content(self, text, tag, props):
-		propstrs = ['{}="{}"'.format(k,props[k]) for k in props]
-		otag = '<{}>'.format(' '.join([tag] + propstrs))
-		ctag = '</{}>'.format(tag)
-		pat = r"""{otag}(.*){ctag}""".format(otag=otag, ctag=ctag)
-		return re.search(pat, text, re.DOTALL).groups()
+	# def get_tag_content(self, text, tag, props):
+	# 	propstrs = ['{}="{}"'.format(k,props[k]) for k in props]
+	# 	otag = '<{}>'.format(' '.join([tag] + propstrs))
+	# 	ctag = '</{}>'.format(tag)
+	# 	pat = r"""{otag}(.*){ctag}""".format(otag=otag, ctag=ctag)
+	# 	return re.search(pat, text, re.DOTALL).groups()
 
 	# This is a terrible way to do this the best way would be to 
 	# have a an algo to scrape the term->seed mappings
@@ -25,14 +26,18 @@ class TPBParser:
 	# and more seeds is better. but here we are
 	def get_magnet_links(self, encoded_term):
 		try:
-			self.get_page(encoded_term+"%20720p")
-			page_text = self.get_page(encoded_term)
-			results = self.get_tag_content(page_text, 'table', {'id':'searchResult'})[0]
-			return re.findall(r'<a href="(magnet:[^"]*)"', results)
+			soup = BeautifulSoup(self.get_page(encoded_term+"%20720p", 'html.parser'))
+			mags = soup.find_all(href=re.compile('magnet'))
+			number=re.compile(r'[0-9]+')
+			maglinks=list()
+			for mag in mags:
+				href = mag['href']
+				seeders,leechers = filter(number.match, mag.parent.parent.stripped_strings)
+				maglinks.append((href,seeders,leechers))
+			best = max(maglinks, key=lambda a: a[2])
+			return best[0]
 		except:
-			self.get_page(encoded_term)
-			page_text = self.get_page(encoded_term)
-			results = self.get_tag_content(page_text, 'table', {'id':'searchResult'})[0]
-			return re.findall(r'<a href="(magnet:[^"]*)"', results)
+			soup = BeautifulSoup(self.get_page(encoded_term, 'html.parser'))
+			return soup.find_all(href=re.compile('magnet'))[0]['href']
 		
 
